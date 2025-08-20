1760136ebd70603e367feb0bd8c4044562297d3b
haritha380 <bandaraindika488@gmail.com>
2025-08-20 18:36:10 +0530

    increse opasity of the background image of dashboard admin

--- wanderlust/src/pages/AdminDashboard.jsx
+++ wanderlust/src/pages/AdminDashboard.jsx
@@ -153,7 +153,7 @@ export default function AdminDashboard() {
     <div className="relative min-h-screen bg-gray-50">
       {/* Background Image */}
       <div 
-        className="fixed inset-0 z-0 bg-center bg-no-repeat bg-cover opacity-10"
+        className="fixed inset-0 z-0 bg-center bg-no-repeat bg-cover opacity-80"
         style={{
           backgroundImage: `url('https://images.unsplash.com/photo-1506905925346-21bda4d32df4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80')`
         }}
